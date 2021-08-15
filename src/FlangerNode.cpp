923405cceeb5d85a33e566460c784197c55b7ded
Nathaniel D'Rozario <nsdrozario@gmail.com>
2021-08-14 19:32:43 -0700

    the linear interpolation now sounds better but isnt quite right

--- src/FlangerNode.cpp
+++ src/FlangerNode.cpp
@@ -40,8 +40,10 @@ void FlangerNode::showGui() {
         imnodes::PopAttributeFlag();
 
         ImGui::DragFloat("Maximum Delay", &max_delay_time, 1.0f, 1.0f, MAX_POSSIBLE_DELAY_MS, "%.3f ms");
-        ImGui::DragFloat("LFO Frequency", &delay_frequency, 0.1f, 0.1f, 1.0f, "%.3f Hz");
+        // ImGui::DragFloat("LFO Frequency", &delay_frequency, 0.1f, 0.1f, 1.0f, "%.3f Hz");
         ImGui::DragFloat("Feedback Gain", &feedback_gain, 1.0f, -144.0f, -1.0f, "%.3f dB");
+
+        // ImGui::Text("%.2f", ((static_cast<float>(max_delay_samples) * 0.5) - 1.0f) * (std::cos(2.0f * 3.1415927f * delay_frequency * internal_timer) + 1));
 
     imnodes::EndNode();
     ImGui::PopItemWidth();
@@ -55,7 +57,7 @@ void FlangerNode::ApplyFX(const float *in, float *out, size_t numFrames, AudioIn
     dsp::clamp(min_delay_time, 1.0f, 30.0f);
     dsp::clamp(max_delay_time, 1.0f, MAX_POSSIBLE_DELAY_MS);
     dsp::clamp(delay_frequency, 0.1f, 1.0f);
-    
+    delay_frequency = 0.001f;
     max_delay_samples = dsp::seconds_to_samples(max_delay_time, info.sample_rate);
 
     if (static_cast<size_t>(static_cast<float>(info.sample_rate) * (max_delay_time / 1000.0f) + 0.5) != delay_buf_size) {
@@ -83,8 +85,7 @@ void FlangerNode::ApplyFX(const float *in, float *out, size_t numFrames, AudioIn
 
     for (size_t i = 0; i < numFrames; i++) {
 
-        float lfo = internal_timer * 500.0f;
-        
+        float lfo = ((static_cast<float>(max_delay_samples) * 0.5) - 1.0f) * (std::cos(2.0f * 3.1415927f * delay_frequency * internal_timer)+1);
         size_t delay_start = static_cast<size_t>(lfo+0.5f);
         size_t delay_end = static_cast<size_t>(lfo);
         float delay_fraction = lfo - std::floor(lfo);
@@ -93,16 +94,15 @@ void FlangerNode::ApplyFX(const float *in, float *out, size_t numFrames, AudioIn
         float y1 = delay_buf[(write_ptr - delay_end + delay_buf_size) % delay_buf_size];
         // dx is 1 unit wide
 
-        float interpolated = ((y1 - y0) * delay_fraction) + y0;
+        float interpolated = ((y1 - y0) * (1.0f-delay_fraction)) + y0;
 
         delay_buf[write_ptr] = in[i];
         write_ptr = (write_ptr + 1) % delay_buf_size;
     
-        out[i] = interpolated;
+        out[i] = in[i] + interpolated;
 
         internal_timer += static_cast<float>(i) / static_cast<float>(info.sample_rate);
-        // internal_timer = std::fmod(internal_timer + (1.0f / delay_frequency), 1.0f / delay_frequency);
-        
+        internal_timer = std::fmod(internal_timer, 1.0f/delay_frequency);
     }
-
+    
 }
