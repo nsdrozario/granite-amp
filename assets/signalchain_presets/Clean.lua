4e4b6d35dcba2db9584da1ed44dc075f38d490c5
Nathaniel D'Rozario <nsdrozario@gmail.com>
2021-10-04 10:15:35 -0700

    implementing loading

---
+++ assets/signalchain_presets/Clean.lua
@@ -0,0 +1,38 @@
+nodes = {
+    {
+        ["type"] = "Overdrive",
+        ["state"] = {
+            ["Gain"]=0,
+            ["Volume"]=-35,
+            ["LPF"]=12000,
+            ["HPF"]=300,
+        }
+    },
+    {
+        ["type"] = "CabSim",
+        ["state"] = {
+            ["HPF"] = 60,
+            ["LPF"] = 12000,
+            ["LowMid"] = {
+                ["freq"] = 250,
+                ["q"] = 4,
+                ["gain"] = 6
+            },
+            ["Mid"] = {
+                ["freq"] = 500,
+                ["q"] = 4,
+                ["gain"] = -12
+            },
+            ["Presence"] = {
+                ["freq"] = 1250,
+                ["q"] = 12,
+                ["gain"] = -6
+            }
+        }
+    }
+}
+
+adjlist = {
+    [3] = 11,
+    [13] = 6
+}
--- src/state.cpp
+++ src/state.cpp
@@ -1,6 +1,6 @@
 #include <state.hpp>
 #include <sstream>
-#include <MiddleNode.hpp>
+#include <Nodes.hpp>
 
 
 std::string adjlist_to_lua() {
@@ -29,7 +29,7 @@ std::string nodes_to_lua() {
     l.script_file("scripts/io_module.lua");
     sol::function serialize_table = l["table_to_str"];
     std::stringstream out;
-    out << "nodes = { ";
+    out << "nodes = {\n";
     // this should traverse in order
     for (auto n : nodes) {
         int id = n.first;
@@ -44,12 +44,59 @@ std::string nodes_to_lua() {
 
 
 void lua_to_adjlist(const sol::table &adjlist_p, const sol::table &adjlist_inward_p) {
-    // please include a size variable
+    for (const auto &o : adjlist_p) {
+        sol::object index = o.first;
+        sol::object value = o.second;
+        if (index.get_type() == sol::type::number && value.get_type() == sol::type::number) {
+            if ( nodes.find((index.as<int>() / 5) * 5) != nodes.end() && nodes.find((value.as<int>() / 5) * 5) != nodes.end()) {
+                adjlist[index.as<int>()] = value.as<int>();
+            }
+        }
+    }
+    for (const auto &o : adjlist_inward_p) {
+        sol::object index = o.first;
+        sol::object value = o.second;
+        if (index.get_type() == sol::type::number && value.get_type() == sol::type::number) {
+            if ( nodes.find((index.as<int>() / 5) * 5) != nodes.end() && nodes.find((value.as<int>() / 5) * 5) != nodes.end()) {
+                adjlist_inward[index.as<int>()] = value.as<int>();
+            }
+        }
+    }
 }
 
 // call this before lua_to_adjlist otherwise you'll get a segfault
 void lua_to_nodes(const sol::table &data) {
     // the first real node is id 10 since input node is id 0 and output onde is id 5
     int node_i = 10;
+    for (const auto &o : data) {
+        sol::object index = o.first;
+        sol::object value = o.second;
+        if (index.get_type() == sol::type::number && value.get_type() == sol::type::table) {
+            std::string node_type = value.as<sol::table>().get_or("type", "invalid");
+            sol::table node_state = value.as<sol::table>().get_or("state", sol::table());
+            if (node_type == "Overdrive") {
+                nodes[node_i] = new guitar_amp::OverdriveNode(node_i, globalAudioInfo);
+            } else if (node_type == "Oscillator") {
+                nodes[node_i] = new guitar_amp::OscillatorNode(node_i, globalAudioInfo);
+            } else if (node_type == "ThreeBandEQ") {
+                nodes[node_i] = new guitar_amp::ThreeBandEQ(node_i, globalAudioInfo);
+            } else if (node_type == "Flanger") {
+                nodes[node_i] = new guitar_amp::FlangerNode(node_i, globalAudioInfo);
+            } else if (node_type == "Delay") {
+                nodes[node_i] = new guitar_amp::DelayNode(node_i, globalAudioInfo);
+            } else if (node_type == "Convolution") {
+                nodes[node_i] = new guitar_amp::ConvolutionNode(node_i, globalAudioInfo);
+            } else if (node_type == "Compressor") {
+                nodes[node_i] = new guitar_amp::CompressorNode(node_i, globalAudioInfo);                
+            } else if (node_type == "CabSim") {
+                nodes[node_i] = new guitar_amp::CabSimNode(node_i, globalAudioInfo);
+            } else if (node_type == "Analyzer") {
+                nodes[node_i] = new guitar_amp::AnalyzerNode(node_i, globalAudioInfo);
+            } else {
 
+            }
+            MiddleNode *real_node;
+            node_i += 5;
+        }
+    }
 }
